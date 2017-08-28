ff881d7420afcc95f76b9dfa64437cfeb9fd8993
Artem Rakov <scoff1k@gmail.com>
2017-08-28 18:29:23 +0100

    delete users controller , added profiles controller instead

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  resources :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :profiles, only: [:show, :index]
   post 'users/:id/match', to: 'matches#create', as: :match
 end
