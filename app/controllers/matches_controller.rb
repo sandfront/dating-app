ce1ffe658c58af4987e70c2313f44ef5a5a853be
Artem Rakov <scoff1k@gmail.com>
2017-08-28 18:11:10 +0100

    created matches controller

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,7 @@
+class MatchesController < ApplicationController
+
+  def create
+    #if it exist we change mutual to true
+    #if doesnt exist create new match
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
 
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
