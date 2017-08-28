1934a968be1cd744bf8157d2b4ad150a207e41a0
Artem Rakov <scoff1k@gmail.com>
2017-08-28 18:02:07 +0100

    created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  post 'users/:id/match', to: 'matches#create', as: :match
 end
