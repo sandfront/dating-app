4b314037fb01bf810ea2e22b15b753dab4484e5e
James Aung <therealaung@gmail.com>
2017-08-29 16:53:56 +0100

    removed raise from user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170829153246) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +89,7 @@ ActiveRecord::Schema.define(version: 20170829153246) do
     t.string "photos"
     t.string "work"
     t.string "subject"
+    t.string "course"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
