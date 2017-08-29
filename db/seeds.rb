94246097b3322e6c660ffbdeab0bea3b6ab60396
ralph kemp <ralphk3mp@gmail.com>
2017-08-29 12:41:27 +0100

    added basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# users needs
 
-# first name
-# last name
-# profile pic
-# description (to add)
-# email
-# encrypted_password
+require 'faker'
+
+20.times do |person|
+  person = User.new()
+  person.first_name = Faker::Name.first_name
+  person.last_name = Faker::Name.last_name
+  person.email = Faker::Internet.email
+  person.password = '123456'
+  person.description = Faker::Lorem.words(20..40)
+  random_men = "https://randomuser.me/api/portraits/men/#{(1..100).to_a.sample}.jpg"
+  random_women = "https://randomuser.me/api/portraits/men/#{(1..100).to_a.sample}.jpg"
+  person.facebook_picture_url = random_men
+  puts 'User created!'
+end
+
+
+
+
 
