176687d75940577bb76ea9ad6743bedcae339fad
Chiara Davies <chiaradavies01@outlook.com>
2017-08-29 15:39:56 +0100

    added a new nav file for edit page it works

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170829104350) do
+ActiveRecord::Schema.define(version: 20170829110910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +32,7 @@ ActiveRecord::Schema.define(version: 20170829104350) do
   end
 
   create_table "matches", force: :cascade do |t|
-    t.boolean "mutual"
+    t.boolean "mutual", default: false
     t.integer "first_user_id"
     t.integer "second_user_id"
   end
@@ -64,8 +63,8 @@ ActiveRecord::Schema.define(version: 20170829104350) do
     t.string "facebook_picture_url"
     t.string "token"
     t.datetime "token_expiry"
-    t.string "course"
     t.text "description"
+    t.string "course"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
