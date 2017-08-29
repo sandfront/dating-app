11a2c288158dbff406becfb4af91e59f8c04b21f
James Aung <therealaung@gmail.com>
2017-08-29 19:04:50 +0100

    added api logic to call photos

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class ProfilesController < ApplicationController
   def index
     @profiles = User.all
@@ -9,6 +12,7 @@ class ProfilesController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    @photos_urls = find_user_fb_photos
   end
 
   def update
@@ -27,5 +31,25 @@ class ProfilesController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :course, user_images: [:id, :user_id, :photo])
+  end
+
+  def find_user_fb_photos
+    url_one = "https://graph.facebook.com/#{current_user.uid}/albums?access_token=#{current_user.token}"
+    albums_hashes = JSON.parse(open(url_one).read)["data"]
+    photos = []
+    albums_hashes.each do |album|
+      url_two = "https://graph.facebook.com/#{album["id"]}/photos?access_token=#{current_user.token}"
+      photos << JSON.parse(open(url_two).read)["data"] # puts the photos of each album into photos array
+    end
+    photos_array = []
+    photos.flatten.first(10).each do |photo|
+      url_three = "https://graph.facebook.com/#{photo["id"]}?fields=images&access_token=#{current_user.token}"
+      photos_array << JSON.parse(open(url_three).read)["images"].first
+    end
+    photos_url = []
+    photos_array.flatten.each do |photo|
+      photos_url << photo["source"]
+    end
+    return photos_url
   end
 end
