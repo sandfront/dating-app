d2f5b3ee5084671db6797566c8de0bf581d5d2a5
Artem Rakov <scoff1k@gmail.com>
2017-09-01 13:45:57 +0100

    added some comments in contoller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,6 +6,6 @@ class ConversationsController < ApplicationController
   def index
     @other_users_with_no_chat = current_user.unstarted_users #this is user_ids array
     @other_users_with_chat = current_user.started_users #this is user_ids array
-    @sorted_convo = current_user.conversations_sorted_by_date
+    @sorted_convos = current_user.conversations_sorted_by_date # this is convos sorted array
   end
 end
