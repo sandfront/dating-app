2a47d7a1c55d6b5efc14ae43c6024f8eaf0ceecd
ralph kemp <ralphk3mp@gmail.com>
2017-09-01 12:54:07 +0100

    done the basic view for messages index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :conversations, only: [:show] do
-    resources :messages, only: [:create]
+  resources :conversations, only: [:index, :show] do
+    resources :messages, only: [ :create]
   end
   resources :profiles, only: [:show, :index, :edit, :update] do
     resources :matches, only: [:create]
