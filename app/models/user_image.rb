f5bab5d3970407dd416d50f1174c1b1f94de86cf
James Aung <therealaung@gmail.com>
2017-09-01 12:29:19 +0100

    added a medium image size fb photo method

--- app/models/user_image.rb
+++ app/models/user_image.rb
@@ -7,6 +7,11 @@ class UserImage < ApplicationRecord
     return JSON.parse(open(base_url).read)["images"].first["source"] # first picks largest size availble
   end
 
+  def medium_url
+    base_url = "https://graph.facebook.com/#{self.photo}?fields=images&access_token=#{self.user.token}" # passed in the id and foreign key
+    return JSON.parse(open(base_url).read)["images"][3]["source"] # first picks largest size availble
+  end
+
   def small_url
     base_url = "https://graph.facebook.com/#{self.photo}?fields=images&access_token=#{self.user.token}" # passed in the id and foreign key
     return JSON.parse(open(base_url).read)["images"].last["source"] # last picks small size
