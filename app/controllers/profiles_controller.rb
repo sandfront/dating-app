d3c37ed03c7f4582bd64224069d6234041777a42
Artem Rakov <scoff1k@gmail.com>
2017-09-02 16:21:40 +0100

    styled show picture page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -33,6 +33,11 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def edit_photo
+    @user = current_user
+    @photo = UserImage.find(params[:photo_id])
+  end
+
   private
 
   def user_params
