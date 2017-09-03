a6d09584f32ee35a9019668f6d5ecdcd8526827a
James Aung <therealaung@gmail.com>
2017-09-03 13:54:30 +0100

    finished swiper js card

---
+++ app/assets/javascripts/swiper-init.js
@@ -0,0 +1,15 @@
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: '.swiper-pagination',
+
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+
+    // And if we need scrollbar
+    scrollbar: '.swiper-scrollbar',
+  })
