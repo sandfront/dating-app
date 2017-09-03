d678cabe086923e16ce8b0ce081e58270b690cb7
James Aung <therealaung@gmail.com>
2017-09-03 14:31:10 +0100

    added all cards swiper

--- app/assets/javascripts/swiper-init.js
+++ app/assets/javascripts/swiper-init.js
@@ -1,15 +1,28 @@
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+var singleCardSwiper = new Swiper ('.swiper-container-single-card', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-    // If we need pagination
-    pagination: '.swiper-pagination',
+  // If we need pagination
+  pagination: '.swiper-pagination',
 
-    // Navigation arrows
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
+  // Navigation arrows
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
 
-    // And if we need scrollbar
-    scrollbar: '.swiper-scrollbar',
-  })
+  // And if we need scrollbar
+  scrollbar: '.swiper-scrollbar',
+})
+
+var allCardsSwiper = new Swiper ('.swiper-container-all-card', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: '.swiper-pagination',
+
+  // Navigation arrows
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})
