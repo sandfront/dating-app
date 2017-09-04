155ed04c7c7f344695e373aca0fd42b11aac50e2
Artem Rakov <scoff1k@gmail.com>
2017-09-04 14:08:05 +0100

    Update match.rb

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,7 +2,6 @@ class Match < ApplicationRecord
   belongs_to :first_user, class_name: 'User'
   belongs_to :second_user, class_name: 'User'
   validates :first_user, uniqueness: { scope: :second_user }
-  validates :second_user
   has_one :conversation
 
   def other_user(test_user)
