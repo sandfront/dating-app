e270987dcfb0c100cf25a0ae7893f4567ea6edea
James Aung <therealaung@gmail.com>
2017-09-04 12:21:08 +0100

    add presence validation for first and second user on match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,8 @@
 class Match < ApplicationRecord
   belongs_to :first_user, class_name: 'User'
   belongs_to :second_user, class_name: 'User'
-  validates :first_user, uniqueness: { scope: :second_user }
+  validates :first_user, uniqueness: { scope: :second_user }, presence: true
+  validates :second_user, presence: true
   has_one :conversation
 
   def other_user(test_user)
