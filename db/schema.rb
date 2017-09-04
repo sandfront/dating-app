bce0ea909bec3a0d08c2237c4e922c01f2b363f1
Artem Rakov <scoff1k@gmail.com>
2017-09-04 18:06:05 +0100

    refactor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904131757) do
+ActiveRecord::Schema.define(version: 20170904164946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,7 +125,7 @@ ActiveRecord::Schema.define(version: 20170904131757) do
     t.text "desc_test3"
     t.date "birthday"
     t.boolean "admin", default: false, null: false
-    t.string "gender_preferences"
+    t.string "gender_preferences", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
