8ed5367992fffbaa437c58c11d4762685d321345
ralph kemp <ralphk3mp@gmail.com>
2017-09-06 15:49:10 +0100

    index updates without reloading

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,23 +2,20 @@ class Message < ApplicationRecord
   validates :content, presence: true, allow_blank: false
   belongs_to :conversation
   belongs_to :user
-  after_create :broadcast_message, :broadcast_message_for_index
+  after_create :broadcast_messages
 
   def from?(some_user)
     user == some_user
   end
 
-  def broadcast_message
+  def broadcast_messages
     ActionCable.server.broadcast("conversation_#{conversation.id}", {
       message_partial: ApplicationController.renderer.render(partial: "shared/message", locals: { message: self, user_is_messages_author: false }),
       current_user_id: user.id
     })
-  end
 
-  def broadcast_message_for_index
-    ActionCable.server.broadcast("user_#{user.id}", {
-      message_partial: ApplicationController.renderer.render(partial: "shared/message", locals: { message: self, user_is_messages_author: false }),
-      current_user_id: user.id
-    })
+    [conversation.match.first_user, conversation.match.second_user].each do |the_user|
+      ActionCable.server.broadcast("user_#{the_user.id}", { "conversations_updated": true })
+    end
   end
 end
